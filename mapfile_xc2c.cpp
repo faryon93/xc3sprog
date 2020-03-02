9ec0b3ad781385821909b3e1ac8498e5e5bbea9c
Maximilian Pachl <m@ximilian.info>
2020-03-02 09:29:09 +0100

    svn upstream r795: https://sourceforge.net/p/xc3sprog/code/795/

---
+++ mapfile_xc2c.cpp
@@ -0,0 +1,247 @@
+/*
+ * Warp Coolrunner II Jedecfile to Bitfiles suitable for programming
+ * and vice- versa
+ *
+ * Needs access to the Xilinx supplied .map files for transformation
+ *
+ * Copyright Uwe Bonnes 2009 bon@elektron.ikp.physik.tu-darmstadt.de
+ *
+*/
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include "mapfile_xc2c.h"
+
+#ifndef MAPDIR
+#if defined (__linux__) || defined(__FreeBSD__) || defined(__MACH__)
+#define MAPDIR "/opt/Xilinx/12.4/ISE_DS/ISE/xbr/data"
+#elif defined(__WIN32__)
+#define MAPDIR "c:"
+#endif
+#endif
+
+#define MAP_ZERO  -1
+#define MAP_ONE   -2
+#define MAP_SPARE -3
+#define MAP_SEC   -10 /* sec_0 = -10, ... , sec_6 = -4 */
+#define MAP_DONE  -12 /* done_0 = -12, done_1 = -11 */
+#define MAP_USER  -44 /* user_0 = -44, ..., user_31 = -13 */
+
+MapFile_XC2C::MapFile_XC2C()
+{
+  map = 0;
+}
+
+MapFile_XC2C::~MapFile_XC2C()
+{
+  if (map)
+    free(map);
+}
+
+int MapFile_XC2C::readmap(FILE *fp)
+{
+  unsigned int i=0, j=0, k=0;
+  int num = 0;
+  char buffer[8];
+  int x;
+  int empty = 2;
+  while ((x = fgetc(fp)) != EOF)
+    {
+      switch (x)
+	{
+	case 0x09:
+	case '\n':
+	  /* Lines with all TABs mark transfer bits and those bits should be
+	  set 0 other empty places should be set 1 */
+	  if     (empty == 2) num = MAP_ZERO; /* Empty line (for now) */
+	  else if(empty == 1) num = MAP_ONE;  /* Empty place */
+	  else empty = 1; /* If not empty reset the value for next token */
+	  if(k > 0) /* String value */
+	    {
+	      if     (strncmp(buffer, "spare", k) == 0) num = MAP_SPARE;
+	      else if(strncmp(buffer, "sec_", k) == 0) num += MAP_SEC;
+	      else if(strncmp(buffer, "done_", k) == 0) num += MAP_DONE;
+	      else if(strncmp(buffer, "user_", k) == 0) num += MAP_USER;
+	      k = 0;
+	    }
+	  /* Store value */
+	  map[i*block_num+j] = num;
+	  num = 0;
+	  j++;
+	  if(x == '\n')
+	    {
+	      j = 0;
+	      i++;
+	      empty = 2;
+	    }
+	  break;
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	  num = 10 * num + (x-'0');
+	  empty = 0;
+	  break;
+	case '\r':
+	  break;
+	case '_':
+	case 'a' ... 'z':
+	  if(k < sizeof(buffer)) buffer[k++] = x;
+	  empty = 0;
+	break;
+	}
+    }
+  return 0;
+}
+
+int MapFile_XC2C::loadmapfile(const char *mapdir, const char *device)
+{
+  FILE *fp;
+  const char * mapfile;
+
+  if (strncasecmp(device, "XC2C32", 6) == 0)
+    {
+      block_size = 260;
+      block_num  = 48;
+      if (strncasecmp(device, "XC2C32A", 7) == 0)
+	mapfile = "xc2c32a";
+      else
+	mapfile = "xc2c32";
+    }
+  else if (strncasecmp(device, "XC2C64", 6) == 0)
+    {
+      block_size = 274;
+      block_num  = 96;
+      if (strncasecmp(device, "XC2C64A", 7) == 0)
+	mapfile = "xc2c64a";
+      else
+	mapfile = "xc2c64";
+    }
+  else if (strncasecmp(device, "XC2C128", 7) == 0)
+    {
+      block_size = 752;
+      block_num  = 80;
+      mapfile = "xc2c128";
+    }
+  else if (strncasecmp(device, "XC2C256", 7) == 0)
+    {
+      block_size = 1364;
+      block_num  = 96;
+      mapfile = "xc2c256";
+    }
+  else if (strncasecmp(device, "XC2C384", 7) == 0)
+    {
+      block_size = 1868;
+      block_num  = 120;
+      mapfile = "xc2c384";
+    }
+  else if (strncasecmp(device, "XC2C512", 7) == 0)
+    {
+      block_size = 1980;
+      block_num  = 160;
+      mapfile = "xc2c512";
+    }
+
+  /* There are two extra rows for security/done and usercode bits*/
+  block_num += 2;
+
+  if (!mapdir)
+    if(!(mapdir = getenv("XC_MAPDIR")))
+      mapdir = MAPDIR;
+
+  mapfilename = (char *) malloc(strlen(mapdir)+strlen(mapfile)+6);
+  if (mapfilename)
+    {
+      strcpy(mapfilename, mapdir);
+      strcat(mapfilename, "/");
+      strcat(mapfilename, mapfile);
+      strcat(mapfilename, ".map");
+    }
+  fp = fopen(mapfilename, "rb");
+  free(mapfilename);
+
+  if (fp == NULL)
+    {
+      fprintf(stderr,"Mapfile %s/%s.map not found: %s\n", mapdir, mapfile,
+	      strerror(errno));
+       return 1;
+    }
+
+  if(map)
+    free (map);
+  map = (int *) malloc(block_size * (block_num) * sizeof(unsigned int));
+  if (map == NULL)
+    return 2;
+  memset(map, 0, block_size * (block_num) * sizeof(unsigned int));
+  readmap(fp);
+  fclose(fp);
+  return 0;
+}
+
+void MapFile_XC2C::jedecfile2bitfile(uint32_t usercode, JedecFile *fuses, BitFile  *bits)
+{
+  int i, j;
+  bits->setLength(block_size*block_num);
+  for (i=0; i<block_num; i++)
+    {
+      for (j=0; j<block_size; j++)
+	{
+	  int bitnum = (i+1)*block_size -j -1;
+	  int fuse_idx = map[j*block_num +i];
+	  int fuse = 1;
+	  switch(fuse_idx)
+	    {
+	      case MAP_ZERO:
+	      case MAP_DONE+1:
+	        fuse = 0;
+		break;
+	      case MAP_ONE:
+	      case MAP_SPARE:
+	      case MAP_SEC ... MAP_SEC+6:
+	      case MAP_DONE:
+	        fuse = 1;
+		break;
+	      case MAP_USER ... MAP_USER+31:
+		fuse = usercode >> (fuse_idx-MAP_USER) & 1;
+		break;
+	      default:
+	        /* xc2c32a.map from 10.1 contain 0 .. 12278 versus 0..12277 */
+	        if (fuse_idx < (int)fuses->getLength())
+		  fuse = fuses->get_fuse(fuse_idx);
+	    }
+	  bits->set_bit(bitnum, fuse);
+	}
+    }
+}
+
+void MapFile_XC2C::bitfile2jedecfile(BitFile  *bits, JedecFile *fuses)
+{
+  int i, j;
+  fuses->setLength(block_size*block_num);
+  int maxnum =0;
+  for (i=0; i<block_num; i++)
+    {
+      for (j=0; j<block_size; j++)
+	{
+	  int bitnum = (i+1)*block_size -j -1;
+	  int bit = 1;
+	  int fuse_idx= map[j*block_num +i];
+	  bit = bits->get_bit(bitnum);
+	  if (fuse_idx>= 0)
+	    {
+	      if (fuse_idx > maxnum)
+		maxnum = fuse_idx;
+	      fuses->set_fuse(fuse_idx, bit);
+	    }
+	}
+    }
+  fuses->setLength(maxnum+1);
+}
---
+++ utilities.cpp
@@ -0,0 +1,170 @@
+#ifndef WIN32
+#include <sys/utsname.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <memory>
+
+#include "ioparport.h"
+#include "iofx2.h"
+#include "ioftdi.h"
+#include "ioxpc.h"
+#include "utilities.h"
+
+extern char *optarg;
+void detect_chain(Jtag *jtag, DeviceDB *db)
+{
+  int num=jtag->getChain();
+  for(int i=0; i<num; i++)
+    {
+      DeviceID id = jtag->getDeviceID(i);
+      fprintf(stderr,"JTAG loc.: %3d  IDCODE: 0x%08lx  ", i, (unsigned long)id);
+      int length = db->idToIRLength(id);
+      if (length > 0)
+        {
+          jtag->setDeviceIRLength(i,length);
+          fprintf(stderr,"Desc: %30s Rev: %c  IR length: %2d\n",
+                  db->idToDescription(id),
+                  (int)(id >> 28) | 'A', length);
+        }
+      else
+        {
+          fprintf(stderr,"not found in '%s'.\n", db->getFile().c_str());
+        }
+    }
+}
+
+int  getIO( std::auto_ptr<IOBase> *io, struct cable_t * cable, char const *dev,
+            char const *serial, bool verbose, bool use_ftd2xx,
+            unsigned int freq)
+{
+    int res = 1;
+    unsigned int use_freq;
+
+    if (!cable)
+    {
+        fprintf(stderr, "No cable selected. You must use -c option."
+                " See xc3sprog -h for more help\n");
+        return 1;
+    }
+
+    if ((freq == 0) || ((cable->freq != 0) && (freq > cable->freq)))
+        use_freq = cable->freq;
+    else
+        use_freq = freq;
+
+  if (cable->cabletype == CABLE_PP)
+    {
+#ifdef __MACH__
+          fprintf(stderr, "Parallel port cables are not supported on OS X.\n");
+          return 1;
+#else
+	  io->reset(new IOParport());
+          io->get()->setVerbose(verbose);
+          res = io->get()->Init(cable, dev, use_freq);
+#endif
+    }
+  else if(cable->cabletype == CABLE_FTDI)
+  {
+      io->reset(new IOFtdi(use_ftd2xx));
+      io->get()->setVerbose(verbose);
+      res = io->get()->Init(cable, serial, use_freq);
+  }
+  else if(cable->cabletype  == CABLE_FX2)
+  {
+      io->reset(new IOFX2());
+      io->get()->setVerbose(verbose);
+      res = io->get()->Init(cable, serial, use_freq);
+  }
+  else if(cable->cabletype == CABLE_XPC)
+  {
+      io->reset(new IOXPC());
+      io->get()->setVerbose(verbose);
+      res = io->get()->Init(cable, serial, use_freq);
+  }
+  else
+  {
+      fprintf(stderr, "Unknown Cable \"%s\" \n", getCableName(cable->cabletype));
+  }
+  return res;
+}
+
+const char *getCableName(int type)
+{
+    switch (type)
+    {
+    case CABLE_PP: return "pp"; break;
+    case CABLE_FTDI: return "ftdi"; break;
+    case CABLE_FX2: return "fx2"; break;
+    case CABLE_XPC: return "xpc"; break;
+    case CABLE_UNKNOWN: return "unknown"; break;
+    default:
+        return "Unknown";
+    }
+}
+
+void
+get_os_name(char *buf, int buflen)
+{
+  memset(buf, 0, buflen);
+#ifdef WIN32
+  snprintf(buf, buflen - 1, "Windows");
+#else
+  struct utsname uts;
+  int ret;
+
+  // Obtain information about current system.
+  memset(&uts, 0, sizeof(uts));
+  ret = uname(&uts);
+  if (ret < 0)
+    snprintf(buf, buflen - 1, "<UNKNOWN>");
+  snprintf(buf, buflen - 1, "%s", uts.sysname);
+#endif
+}
+
+void xc3sprog_Usleep(unsigned int usec)
+{
+#if defined(__WIN32__)
+  /* Busy wait, as scheduler prolongs all waits for to least 10 ms
+     http://sourceforge.net/apps/trac/scummvm/browser/vendor/freesci/   \
+     glutton/src/win32/usleep.c?rev=38187
+  */
+  LARGE_INTEGER lFrequency;
+  LARGE_INTEGER lEndTime;
+  LARGE_INTEGER lCurTime;
+
+  QueryPerformanceFrequency (&lFrequency);
+  if (lFrequency.QuadPart)
+  {
+      QueryPerformanceCounter (&lEndTime);
+      lEndTime.QuadPart += (LONGLONG) usec * lFrequency.QuadPart / 1000000;
+      do
+      {
+          QueryPerformanceCounter (&lCurTime);
+          if (usec > 11000)
+              Sleep(0);
+      } while (lCurTime.QuadPart < lEndTime.QuadPart);
+  }
+#else
+  usleep(usec);
+#endif
+}
+
+
+/* Split string on delimiting character. */
+std::vector<std::string> splitString(const std::string& s, char delim)
+{
+  std::vector<std::string> res;
+  for (std::string::size_type i = 0, n = s.size(); i < n; )
+    {
+      std::string::size_type k = s.find(delim, i);
+      if (k == std::string::npos)
+        k = n;
+      res.push_back(s.substr(i, k - i));
+      i = k + 1;
+    }
+  return res;
+}
+
